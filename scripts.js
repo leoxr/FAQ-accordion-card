c5d14e4c2b34df2638bae19b4af516e651398ed4
leoxr <101909426+leoxr@users.noreply.github.com>
2022-04-23 02:22:16 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,29 +1,23 @@
-const faqEl = document.getElementById('faq')
+const faqCont = document.querySelectorAll(".faq-ask-container");
 
-faqEl.addEventListener('click', (e) => {
-    
-    if(e.target.classList.contains('faq__arrow-down')) {
-
-        e.target.classList.remove('faq__arrow-down');
-        e.target.classList.add('faq__arrow-up');
-        // console.dir(e.target.parentNode.children[0].children[1])
-        e.target.parentNode.children[0].children[0].classList.toggle('faq-bold');
-        e.target.parentNode.children[0].children[1].classList.toggle('hidden');
-    
-    } else if(e.target.classList.contains('faq__arrow-up')) {
-
-        e.target.classList.remove('faq__arrow-up');
-        e.target.classList.add('faq__arrow-down');
-        // console.dir(e.target.parentNode.children[0].children[1])
-        e.target.parentNode.children[0].children[1].classList.toggle('hidden');
-        e.target.parentNode.children[0].children[0].classList.toggle('faq-bold');
-
+for (const i of faqCont) {
+  i.addEventListener("click", () => {
+    //   console.dir(i)
+    if (i.lastElementChild.classList.contains("faq__arrow-down")) {
+      i.lastElementChild.classList.remove("faq__arrow-down");
+      i.lastElementChild.classList.add("faq__arrow-up");
+      i.children[0].children[0].classList.toggle("faq-bold");
+      i.children[0].children[1].classList.toggle("hidden");
+      i.lastElementChild.classList.add("transition1");
+    } else {
+      i.lastElementChild.classList.remove("faq__arrow-up");
+      i.lastElementChild.classList.add("faq__arrow-down");
+      i.children[0].children[0].classList.toggle("faq-bold");
+      i.children[0].children[1].classList.toggle("hidden");
+      i.lastElementChild.classList.add("transition1");
     }
-
-
-});
-
-
+  });
+}
  
 
 
